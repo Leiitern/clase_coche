eeed07e38c820e527671c421c6fd3a3d033e21ee
Leiitern <mateogamer2001@gmail.com>
2022-04-27 12:13:47 -0300

    Func suma

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,27 @@
 
 public class Main {
     public static void main(String[] args) {
-        suma(1,2,3);
+
+        suma(1,2,3); //Invoco la funcion suma
+
+        Coche miCoche = new Coche(); //Objeto Coche
+        miCoche.incrementaPuertas(); //Incrementa las puertas en 1
+
+        System.out.println(miCoche.puertas); //Muestro por pantalla el incremento
     }
 
-    public static void suma(int a, int b, int c){
+    public static class Coche{ // Clase Coche
+        int puertas = 0;
+
+        public void incrementaPuertas(){
+            puertas++;
+        }
+    }
+    public static void suma(int a, int b, int c){ //Funcion Suma
         int resultado = a + b + c;
 
         System.out.println(resultado);
     }
-}
 
+
+}
